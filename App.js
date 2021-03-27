28c7aee11cb2a4430b638f6c735805f2a7b83082
TECHNICALFUNANDFACT <78973509+TECHNICALFUNANDFACT@users.noreply.github.com>
2021-03-27 21:08:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ' ./App.css';
+
+const App = () => {
+    return ( <
+        ChatEngine height = "100vh"
+        projectID = "10a672c7-e439-4503-aace-10aeb7d4d750"
+        userName = "javascriptmastery"
+        userSecret = "123123"
+
+
+
+
+        /
+        >
+    );
+
+}
+export default App;
